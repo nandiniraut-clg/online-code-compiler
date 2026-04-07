941cc948d8dba9c064e13d2dfc786ba099b46f59
nandiniraut-clg <nandini.raut@cumminscollege.in>
2026-04-07 12:24:26 +0530

    Initial backend setup with Spring Boot structure

---
+++ backend/src/test/java/com/compiler/online_code_compiler/OnlineCodeCompilerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compiler.online_code_compiler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineCodeCompilerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
